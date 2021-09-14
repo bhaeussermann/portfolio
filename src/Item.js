e4bc93dd03b6be5233ac401f8771a8c9b4cafad5
Bernhard Häussermann <bernhardgh@icloud.com>
2021-09-14 20:35:52 +0200

    Add styling for dark mode

--- src/Item.js
+++ src/Item.js
@@ -6,7 +6,11 @@ export default function Item(props) {
       <div className="row">
         <a className="app-link" href={props.info.appUrl} target="_blank" rel="noreferrer">{props.info.title}</a>
         <a className="repo-link" href={props.info.repoUrl} target="_blank" rel="noreferrer">
-          <img src="assets/github.png" alt="Repo on GitHub" />
+          <picture>
+            <source srcset="assets/github-light.png" media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)" />
+            <source srcset="assets/github-dark.png" media="(prefers-color-scheme: dark)" />
+            <img alt="Repo on GitHub" />
+          </picture>
         </a>
         <div className="expander" onClick={() => props.setExpanded(!props.expanded)}></div>
       </div>
